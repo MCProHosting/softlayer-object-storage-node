12a552fe69a15ababef8ce4b24c70958a699e082
Mark Dietzer <mriq91@gmail.com>
2014-10-15 20:57:27 +0200

    Add listContainers

--- app.js
+++ app.js
@@ -2,13 +2,16 @@ var storage = require("./lib/main.js");
 var config = require("./config.js");
 
 storage.login(config.endpoint, config.username, config.api_key,  function(connection) {
+	connection.listContainers(function(success, containers) {
+		console.log("L", success, containers);
+	});
 	var container = connection.getContainer(config.container);
-	container.put({
+	container.putFile({
 		filename: "test/test.txt",
 		contents: "Test"
 	}, function(success, body) {
 		console.log("P", success, body);
-		container.get({
+		container.getFile({
 			filename: "test/test.txt"
 		}, function(success, body) {
 			console.log("G", success, body);
