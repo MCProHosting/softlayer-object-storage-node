8bd4c8fb7e349568e817838fb79565d94156cbb7
Mark Dietzer <mriq91@gmail.com>
2014-10-15 17:00:01 +0200

    Made uploading work for files as well

--- app.js
+++ app.js
@@ -2,16 +2,16 @@ var storage = require("./lib/main.js");
 var config = require("./config.js");
 
 storage.login(config.endpoint, config.username, config.api_key,  function(connection) {
-	var container = connection.getContaioner(config.container);
+	var container = connection.getContainer(config.container);
 	container.put({
 		filename: "test/test.txt",
-		contents: "hai"
+		contents: "Test"
 	}, function(success, body) {
 		console.log("P", success, body);
 		container.get({
 			filename: "test/test.txt"
 		}, function(success, body) {
-			console.log("D", success, body);
+			console.log("G", success, body);
 		});
 	});
 });
