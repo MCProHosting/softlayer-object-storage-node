82b337cb271cdc55986171226192cb508e9d0f68
Mark Dietzer <mriq91@gmail.com>
2014-10-15 14:53:38 +0200

    Add initial things. Basic workings done

---
+++ app.js
@@ -0,0 +1,17 @@
+var storage = require("./storage.js");
+var config = require("./config.js");
+
+storage.login(config.endpoint, config.username, config.api_key,  function(connection) {
+	var container = connection.getContaioner(config.container);
+	container.put({
+		filename: "test/test.txt",
+		contents: "hai"
+	}, function(success, body) {
+		console.log("P", success, body);
+		container.get({
+			filename: "test/test.txt"
+		}, function(success, body) {
+			console.log("D", success, body);
+		});
+	});
+});
---
+++ config.example.js
@@ -0,0 +1,6 @@
+module.exports = {
+	endpoint: "https://dal05.objectstorage.softlayer.net/auth/v1.0",
+	container: "*",
+	username: "*",
+	api_key: "*",
+}
